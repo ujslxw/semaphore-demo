b3597f9729793300c26d859ad4e1046ab1b623d6
Denis Gukov <denguk@gmail.com>
2025-01-10 13:32:31 +0500

    feat: update terraform demo

--- demo.tf
+++ demo.tf
@@ -1,6 +1,24 @@
-provider "null" {
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
 }
 
-resource "null_resource" "example" {
-  # This resource does nothing
+provider "local" {}
+
+# Define a local file
+resource "local_file" "example" {
+  filename = "${path.module}/example.txt"
+  content  = <<EOT
+This is an example file created by Terraform.
+You can customize its content.
+EOT
+}
+
+# Output the file path
+output "file_path" {
+  value = local_file.example.filename
 }
